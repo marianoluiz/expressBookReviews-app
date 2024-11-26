184165056f2e90cf8ea8a7e6ff3da561cecd38cb
marianoluiz <kmarianu@gmail.com>
2024-11-26 18:25:07 +0800

    added comments explanations

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# coding-project-template
+API Overview
+Public Routes: These routes do not require authentication.
+
+GET /: Fetch the list of books.
+GET /isbn/:isbn: Fetch book details based on ISBN.
+GET /author/:author: Fetch books by a specific author.
+GET /title/:title: Fetch books by title.
+POST /register: Register a new user.
+Authenticated Routes: These routes require authentication.
+
+POST /customer/login: Login for registered users.
+PUT /customer/auth/review/:isbn: Add or modify a book review.
+DELETE /customer/auth/review/:isbn: Delete a book review.
--- final_project/index.js
+++ final_project/index.js
@@ -12,6 +12,23 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 // Middleware to authenticate customers
 app.use("/customer/auth/*", function auth(req,res,next){
   if(req.session.authorization){
+    //req . session . authorization
+
+    /* 
+      {
+        "cookie": {
+          "originalMaxAge": null,
+          "expires": null,
+          "httpOnly": true,
+          "path": "/"
+        },
+        "authorization": {
+          "accessToken": "your-jwt-token"
+        },
+        "userId": "12345",
+        "username": "john_doe"
+      }
+    */
     let token = req.session.authorization['accessToken'];
 
     jwt.verify(token, "access", (err, user) => {
@@ -33,5 +50,7 @@ const PORT =5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
+
+//Middleware in Express.js is executed in the order it is defined. The authentication middleware only applies to routes that match /customer/auth/*. Since / does not match this pattern, it bypasses the authentication middleware. so i can access "/" without registering in.
 
 app.listen(PORT,()=>console.log("Server is running"));
